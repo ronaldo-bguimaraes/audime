ca2c04e3c68444c7e2d657cf4ef5aba5ff89eb95
Ronaldo Guimarães <ronaldo.bguimaraes@outlook.com>
2026-05-28 00:30:56 -0400

    feat: add web scripts and react router basic config

--- README.md
+++ README.md
@@ -10,8 +10,14 @@ npm install
 
 ## Executar web
 
+### To Build
 ```bash
-npm run dev -w @audime/web
+npm run dev:api
+```
+
+### To Dev
+```bash
+npm run dev:api
 ```
 
 ## Executar api
@@ -19,4 +25,17 @@ npm run dev -w @audime/web
 ```bash
 npm run dev -w @audime/api
 npm run build -w @audime/api
+```
+
+
+## Configurar deploy
+
+### To Build
+```bash
+npm ci && npm run build:api
+```
+
+### To Start API
+```bash
+npm run start:api
 ```
--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router";
 
-createRoot(document.getElementById('root')!).render(
+import "./index.css";
+
+import Home from "./pages/Home.tsx";
+import NotFound from "./pages/NotFound.tsx";
+
+const root = document.getElementById("root")!;
+
+createRoot(root).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
-)
+);
