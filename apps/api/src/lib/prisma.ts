a81839c87ff1410c262018f3d93504155a17498a
Ronaldo Guimarães <ronaldo.bguimaraes@outlook.com>
2026-05-30 17:07:00 -0400

    feat: move prisma config file

--- apps/api/src/lib/prisma.ts
+++ apps/api/src/lib/prisma.ts
@@ -1,5 +1,8 @@
-import { PrismaClient } from "../generated/prisma/client"
-import { PrismaPg } from "@prisma/adapter-pg"
+import "dotenv/config";
+import { PrismaClient } from "../generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL })
-export const prisma = new PrismaClient({ adapter })
+const connectionString = process.env.DATABASE_URL;
+
+const adapter = new PrismaPg({ connectionString });
+export const prisma = new PrismaClient({ adapter });
