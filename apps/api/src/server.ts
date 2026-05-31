d76b6bce377165b2cccef64c160ca7453edf9e56
Ronaldo Guimarães <ronaldo.bguimaraes@outlook.com>
2026-05-31 11:05:27 -0400

    fix: set host in server.ts

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -2,11 +2,11 @@ import "dotenv/config";
 
 import { app } from "./app";
 
-export const API_PORT = Number.parseInt(process.env.PORT ?? "3333");
+export const SERVER_PORT = Number.parseInt(process.env.PORT ?? "3333");
 
 const start = async () => {
   try {
-    await app.listen({ port: API_PORT });
+    await app.listen({ host: "0.0.0.0", port: SERVER_PORT });
   } catch (err) {
     app.log.error(err);
     process.exit(1);
